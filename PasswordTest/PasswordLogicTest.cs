4623873d06e877fd911713e3b2a75c5c18e3f69c
Camara Ibrahima <camara@eurisnet.it>
2018-04-26 11:05:48 +0200

    -Fase Rossa: password should be larger than 8 chars

---
+++ PasswordTest/PasswordLogicTest.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PasswordLogic;
+
+namespace PasswordTest
+{
+    [TestFixture]
+    public class PasswordLogicTest
+    {
+        [TestCase("")]
+        public void TestClassVerify(string password)
+        {           
+            Assert.IsFalse(PasswordVerifier.Verify(password));
+        }
+    }
+}
