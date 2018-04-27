0bcdca0cde8054c35b7088904cc2599cbb40eadd
Camara Ibrahima <camara@eurisnet.it>
2018-04-27 11:58:50 +0200

    Lower case test

---
+++ PasswordLogic/LowerCasePasswordVerifier.cs
@@ -0,0 +1,20 @@
+﻿using System.Text.RegularExpressions;
+
+namespace PasswordLogic
+{
+    public class LowerCasePasswordVerifier : PasswordVerifierHandler
+    {
+        public override bool Verify(string password)
+        {
+            if (!Regex.IsMatch(password, @"[a-z]"))
+            {
+                return false;
+            }
+            else if (successor != null)
+            {
+                return successor.Verify(password);
+            }
+            return true;
+        }
+    }
+}
--- PasswordTest/PasswordLogicTest.cs
+++ PasswordTest/PasswordLogicTest.cs
@@ -9,6 +9,7 @@ namespace PasswordTest
         PasswordVerifierHandler _nullOrEmpty;
         PasswordVerifierHandler _large8Char;
         PasswordVerifierHandler _upperCase;
+        PasswordVerifierHandler _lowerCase;
 
         [SetUp]
         public void OnStart()
@@ -16,13 +17,15 @@ namespace PasswordTest
             _nullOrEmpty = new NullOrEmptyPasswordVerifier();
             _large8Char = new Larger8CharPasswordVerifier();
             _upperCase = new UpperCaseCharPasswordVerifier();
+            _lowerCase = new LowerCasePasswordVerifier();
             _nullOrEmpty.SetSuccessor(_large8Char);
             _large8Char.SetSuccessor(_upperCase);
+            _upperCase.SetSuccessor(_lowerCase);
         }
         [TestCase("")]
         [TestCase(null)]
         [TestCase("mancaunamaiuscola")]
-        //[TestCase("MANCAUNAMAIUSCOLA")]
+        [TestCase("MANCAUNAMAIUSCOLA")]
         //[TestCase("mancaUNnumero")]
         //[TestCase("mancaunCarattereSpeciale1")]
         public void TestVerifyIsFalse(string password)
