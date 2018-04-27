fe997bfcc18127fa532cdd611736fa9d075c1531
Camara Ibrahima <camara@eurisnet.it>
2018-04-27 12:05:38 +0200

    Number test

--- PasswordTest/PasswordLogicTest.cs
+++ PasswordTest/PasswordLogicTest.cs
@@ -10,6 +10,7 @@ namespace PasswordTest
         PasswordVerifierHandler _large8Char;
         PasswordVerifierHandler _upperCase;
         PasswordVerifierHandler _lowerCase;
+        PasswordVerifierHandler _number;
 
         [SetUp]
         public void OnStart()
@@ -18,15 +19,18 @@ namespace PasswordTest
             _large8Char = new Larger8CharPasswordVerifier();
             _upperCase = new UpperCaseCharPasswordVerifier();
             _lowerCase = new LowerCasePasswordVerifier();
+            _number = new NumberPassworVerifier();
             _nullOrEmpty.SetSuccessor(_large8Char);
             _large8Char.SetSuccessor(_upperCase);
             _upperCase.SetSuccessor(_lowerCase);
+            _lowerCase.SetSuccessor(_number);
+
         }
         [TestCase("")]
         [TestCase(null)]
         [TestCase("mancaunamaiuscola")]
         [TestCase("MANCAUNAMAIUSCOLA")]
-        //[TestCase("mancaUNnumero")]
+        [TestCase("mancaUNnumero")]
         //[TestCase("mancaunCarattereSpeciale1")]
         public void TestVerifyIsFalse(string password)
         {           
